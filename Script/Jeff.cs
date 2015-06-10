cddd5b246de2501a84d6c34bc3cc6560fd13409f
Abhinav Bhandari <abhinavbhandari4@gmail.com>
2015-06-10 19:38:17 +0100

    minor tweaks

--- Script/Jeff.cs
+++ Script/Jeff.cs
@@ -1,11 +1,48 @@
 using System;
-public class Program
-{public static void Main(){Console.WriteLine("Hello and welcome. My name is Jeff the AI");defs();}
-public static void defs(){
-	
-reply("moo`oink`woof`bark`baa", "mooooooooo!`Indeed, moooo`hahaha LOL moOoooooOOOOoo!");
-reply("hello", "Hello there! How are you on this fine day?`Hi dude! What's up?`Howdy! Fine weather we are having don't you agree?`Hello there! Isn't the weather just spectacular today?");
 
-}public static void reply(string wha, string rawans){string[] answer = rawans.Split('`'); string[] replytowhat = wha.Split('`'); foreach(var item in replytowhat){
-if(Console.ReadLine().ToLower().Contains(item) == true){Random rnd = new Random();int rand = rnd.Next(answer.Length);Console.WriteLine(answer[rand]);} defs();}
-}}
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello and welcome. My name is Jeff the AI");
+		defs();
+	}
+
+	public static void defs()
+	{
+		reply("moo`oink`woof`bark`baa", "mooooooooo!`Indeed, moooo`hahaha LOL moOoooooOOOOoo!");
+		reply("hello", "Hello there! How are you on this fine day?`Hi dude! What's up?`Howdy! Fine weather we are having don't you agree?`Hello there! Isn't the weather just spectacular today?");
+	}
+
+	public static void reply(string wha, string rawans)
+	{
+		int no = 0;
+		bool has = false;
+		string[] answer = rawans.Split('`');
+		string[] replytowhat = wha.Split('`');
+		Random rnd = new Random();
+		int rand = rnd.Next(answer.Length);
+		string wotsay = Console.ReadLine();
+		da:
+		if(wotsay.Contains(replytowhat[no]) == true){
+			has = true;
+		} else {
+			has = false;
+		}
+		
+		
+		if(has == true){
+			Console.WriteLine(answer[rand]);
+			no = 0;
+			defs();
+		} else if ( has == false){
+			if(no >= replytowhat.Length){
+				//not contain BRO
+				Console.WriteLine("DOEWAS NOT CONAITAIN BROOO");
+			} else if(no <= replytowhat.Length){
+				no++;
+			}
+			goto da;
+		}
+	}
+}
