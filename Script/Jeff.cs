ba70c5f489e91ec40945160498d5b76faf195387
Abhinav Bhandari <abhinavbhandari4@gmail.com>
2015-06-10 10:07:06 +0100

    Jeff is born

--- Script/Jeff.cs
+++ Script/Jeff.cs
@@ -5,7 +5,12 @@ public class Program
 	public static void Main()
 	{
 		Console.WriteLine("Hello and welcome. My name is Jeff the AI");if(Console.ReadLine().ToLower().Contains("moo") == true){Console.WriteLine("moo to you too, fella!");}
-		if(Console.ReadLine().ToLower().Contains("hello") == true)
+		if(Console.ReadLine().ToLower().Contains("hello") == true){
+			Random rnd = new Random();
+			int rand = rnd.Next(2);
+			if(rand == 0){ Console.WriteLine("Hello there dude! Awesome weather we're having - do you not agree?"); }
+			if(rand == 1){ Console.WriteLine("Hi! How are you doing?");}
+		}
 			
 	}
 }
