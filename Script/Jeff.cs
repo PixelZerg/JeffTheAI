42a052ea6cc9de1e9d4c5dbcb1c6b4ab20df724d
Abhinav Bhandari <abhinavbhandari4@gmail.com>
2015-06-10 10:03:11 +0100

    Jeff is born

---
+++ Script/Jeff.cs
@@ -0,0 +1,11 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello and welcome. My name is Jeff the AI");if(Console.ReadLine().ToLower().Contains("moo") == true){Console.WriteLine("moo to you too, fella!");}
+		if(Console.ReadLine().ToLower().Contains("hello") == true)
+			
+	}
+}
