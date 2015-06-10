75281781951ad912cf882beb6bfc527c4e0bb8b6
Abhinav Bhandari <abhinavbhandari4@gmail.com>
2015-06-10 22:42:54 +0100

    translated Jeff to Nepalese to English
    Translated to Nepalese and back to English again? I tried to use the closest translations I could - notice some differences between the English version and the English in the comments?

--- Script/Jehfu.cs
+++ Script/Jehfu.cs
@@ -11,10 +11,15 @@ public class Program
 
 
         replya("moo`oink`bau`bhau`baa", "mooooooooo!`Ho, mooooo`hahaha khasto hasuteyo moOoooooOOOOoo!");
+        //-> moo`yes, moo` haha how funny mooooo
         replya("namaste`hi`howdy`khasto cha`kasto cha`khasto bhakocha`konninchi`", "Ke cha thya! Yo dami din ma hajur khasto hunu'ncha`Eh yarr! Ke cha?`Hello! Hami sunga tha dami weatheru ei rakhekocha hai?`Namaste! Aja ko weatheru kathi dami cha hai?");
+        //namaste`hi`howdy`how's it`how's it`how's it going`konninchiwa -> how's it there!? On this awesome day how are you doing?`m8! How's it?`Hello! We are having awesome weather right?` Greetings! The weather of today is so awesome right?
         replya("malai thacha`malai thapa ko chu`maile thapayeko cha", "Hai?!!`Auh - timi malai bujchus!`Eh auh - malai thacha timi kasto cha`Ma timi sangai chu tyo wala ma");
+        //i know` i have known(?)`i have been knowing(?) -> Right?!! Yeah - you understand me!` Oh yeah - I know you are`I am with you on that one
         replya("lol`haha`teehee`heehee`ahah`lmao`rofl`lmfao`:d`cx", "kasto hasutne hai?`hasne ramro ho`Eh, alik kati hasutne thyo :/");
+        // -> how very funny, right?`laughing is good`Oh, it was a bit funny (i couldn't figure out how to say "I suppose")
         replya("Auh`ho`uh`hai`thei tha`hola`yah`ye`ya", "Positivity bhaneko egdum ramro ho, tapai lai egdum suyoge gurcha`Jailepani yo sunsar lai positively hernu huncha hola");
+        //Yeah`indeed`yea`right?`exactly`really?`yah`ye`ya -> Positivity is very good, it will service you a lot`May you always look at/upon this world positively
 
 
 
