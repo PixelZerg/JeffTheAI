dd8c60c3de17d40d9cc10c514daf2dae551af58e
Abhinav Bhandari <abhinavbhandari4@gmail.com>
2015-06-10 16:23:55 +0100

    reply method now cuts up strings into arrays

--- Script/Jeff.cs
+++ Script/Jeff.cs
@@ -1,44 +1,11 @@
 using System;
-					
 public class Program
-{
-	public static void Main()
-	{
-		string[] array;
-		
-		Console.WriteLine("Hello and welcome. My name is Jeff the AI");
-		
-		/*if(Console.ReadLine().ToLower().Contains("moo") == true){Console.WriteLine("moo to you too, fella!");}
-		if(Console.ReadLine().ToLower().Contains("hello") == true){
-			Random rnd = new Random();
-			int rand = rnd.Next(2);
-			if(rand == 0){ Console.WriteLine("Hello there dude! Awesome weather we're having - do you not agree?"); }
-			if(rand == 1){ Console.WriteLine("Hi! How are you doing?");}*/
-		array = new string[2];
-		array[0] = "Moo to you too!";
-		array[1] = "I agree, moo indeed";
-		reply("moo", array);
-		}
-			
+{public static void Main(){Console.WriteLine("Hello and welcome. My name is Jeff the AI");defs();}
+public static void defs(){
+	
+reply("moo`oink`woof`bark`baa", "mooooooooo!`Indeed, moooo`hahaha LOL moOoooooOOOOoo!");
+reply("hello", "Hello there! How are you on this fine day?`Hi dude! What's up?`Howdy! Fine weather we are having don't you agree?`Hello there! Isn't the weather just spectacular today?");
 
-		
-		public static void reply(string replytowhat, string[] answer)
-		{
-			if(Console.ReadLine().ToLower().Contains(replytowhat) == true){
-			Random rnd = new Random();
-			int rand = rnd.Next(answer.Length);
-				int no = 0;
-				/*foreach(var item in answer){
-					if(no == rand){
-						//do the answer
-						Console.WriteLine(item);
-						//no++;
-					} else {
-						no++;
-					}
-				}*/
-				Console.WriteLine(answer[rand]);
-		}
-			
-}
-}
+}public static void reply(string wha, string rawans){string[] answer = rawans.Split('`'); string[] replytowhat = wha.Split('`'); foreach(var item in replytowhat){
+if(Console.ReadLine().ToLower().Contains(item) == true){Random rnd = new Random();int rand = rnd.Next(answer.Length);Console.WriteLine(answer[rand]);} defs();}
+}}
